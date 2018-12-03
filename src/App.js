4876c45f541c9e085988659499af3fdc4024d8b8
{salmamedhat} <{salmamedhat272@gmail.com}>
2018-12-03 17:59:17 +0200

    merged with the master

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import firebase from './firebase.js';
+import ImageUploader from "react-images-upload";
+
 
 class App extends Component {
   constructor() {
@@ -10,7 +12,8 @@ class App extends Component {
       foundItem: '',
       removalCode: '',
       foundIn:'',
-      items: []
+      items: [],
+      tempImage: []
     }
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -60,24 +63,60 @@ componentDidMount() { // to fetch items from DB
   });
 }
   
-  claimMine(itemId) {
+  deleteItem(itemId) {
   const itemRef = firebase.database().ref(`/items/${itemId}`);
   itemRef.remove();
+}
+
+onDrop = picture => {
+  this.setState({
+    tempImage: this.state.tempImage.concat(picture)
+  });
+};
+sendEmail() {
+  var postmark = require("postmark");
+  var client = new postmark.Client("d15218b9-7bbc-4ec8-93ed-1d71b58a837d");
+  client.sendEmail({
+    From: "noreply@ifound.com",
+    To: "ahmed11rihan@gmail.com",
+    Subject: "Test",
+    TextBody: "Hello from Postmark!"
+  });
 }
  
   render() {
     
     return (
       <div className='app'>
-        <header>
-            <div className='wrapper'>
-              <h1>Lost and found</h1>
-              
-            </div>
-        </header>
+
+                  <div
+            style={{
+              textAlign: "center",
+              paddingTop: "30px",
+              paddingBottom: "30px",
+              backgroundColor: "#5e548e",
+              color: "white"
+            }}
+          >
+            <h1>Lost and Found App</h1>
+          </div>
+
         <div className='container'>
           <section className='add-item'>
+          <h1>Add an item</h1>
+              <hr />
               <form onSubmit={this.handleSubmit}>
+              <div className="form-group">
+                  <label for="imageUploader">Upload Image</label>
+                  <ImageUploader
+                    withIcon={true}
+                    id="imageUploader"
+                    buttonText="Choose image"
+                    onChange={this.onDrop}
+                    imgExtension={[".jpg", ".gif", ".png", ".gif"]}
+                    maxFileSize={5242880}
+                  />
+                </div>
                 <input type="text" name="posterEmail" placeholder="What's your Email?" onChange={this.handleChange} value={this.state.posterEmail} />
                 <input type="text" name="foundItem" placeholder="What did you find?" onChange={this.handleChange} value={this.state.foundItem} />
                 <input type="text" name="foundIn" placeholder="Where did you find it?" onChange={this.handleChange} value={this.state.foundIn} />
@@ -87,14 +126,46 @@ componentDidMount() { // to fetch items from DB
               </form>
           </section>
           <section className='display-item'>
-  <div className="wrapper">
+  <div className="wrapper" >
+  <h1>List of Items</h1>
+              <hr />
     <ul>
       {this.state.items.map((item) => {
         return (
           <li key={item.id}>
-            <h3>{item.foundItem}</h3>
-            <p>Found in: {item.foundIn}</p>
-            <button onClick={() => this.claimMine(item.id)}>This is mine!!</button>
+            <img
+              className="card-img-top"
+              src={item.imageUrl}
+              alt="Card image cap"
+              style={{ width: "200px" }}
+            />
+            <div className="card-body">
+              <h5 className="card-title" style={{ textAlign: "center" }}>
+                {item.foundItem}
+              </h5>
+              <p>Found in: {item.foundIn}</p>
+              <a
+                href="#"
+                onClick={this.sendEmail}
+                className="btn btn-primary"
+                style={{ width: "100%" }}
+                
+              >
+              This is mine!!
+              </a>
+              <br />
+              <br />
+              <a
+              href="#"
+              onClick={() => this.deleteItem(item.id)}
+              className="btn btn-danger"
+              style={{
+              width: "100%"
+               }}
+              >
+               Delete
+              </a>
+            </div>
           </li>
         )
       })}
