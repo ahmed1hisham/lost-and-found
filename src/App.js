e0c249b8a0d65032eeee75956068ec05632b74a5
{salmamedhat} <{salmamedhat272@gmail.com}>
2018-12-03 16:15:21 +0200

    event handlers for input

--- src/App.js
+++ src/App.js
@@ -3,7 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      posterEmail: '',
+      foundItem: '',
+      removalCode: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+ 
   render() {
+    
     return (
       <div className='app'>
         <header>
@@ -15,9 +32,9 @@ class App extends Component {
         <div className='container'>
           <section className='add-item'>
               <form>
-                <input type="text" name="posterEmail" placeholder="What's your Email?" />
-                <input type="text" name="foundItem" placeholder="What did you find?" />
-                <input type="text" name="removalCode" placeholder="Choose a code for deletion" />
+                <input type="text" name="posterEmail" placeholder="What's your Email?" onChange={this.handleChange} value={this.state.posterEmail} />
+                <input type="text" name="foundItem" placeholder="What did you find?" onChange={this.handleChange} value={this.state.foundItem} />
+                <input type="text" name="removalCode" placeholder="Choose a code for deletion" onChange={this.handleChange} value={this.state.removalCode} />
                 <button>Add Item</button>
               </form>
           </section>
