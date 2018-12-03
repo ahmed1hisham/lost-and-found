573ca0b879d60904e0dccd84c5e2bccf35aa324c
Ahmed Hisham Rihan <36399893+ahmed1hisham@users.noreply.github.com>
2018-12-03 15:56:25 +0200

    added firebase to the app and created the project

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+  import firebase from 'firebase'
+  var config = {
+    apiKey: "AIzaSyAKTKopqBgl-H7EvwcpgyLS-vZ-CpCjiKM",
+    authDomain: "lost-and-found-final.firebaseapp.com",
+    databaseURL: "https://lost-and-found-final.firebaseio.com",
+    projectId: "lost-and-found-final",
+    storageBucket: "",
+    messagingSenderId: "602059640798"
+  };
+  firebase.initializeApp(config);
+  export default firebase;
